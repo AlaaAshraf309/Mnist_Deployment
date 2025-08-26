b863e2a576a10b354dc4dd9ed85d8177f765272a
Alaa <alaahegab309@gmail.com>
2025-08-26 12:10:57 +0300

    edit app

--- app.py
+++ app.py
@@ -1,43 +1,17 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.utils import to_categorical 
+from tensorflow.keras.models import load_model
 from PIL import Image, ImageOps
 
 st.set_page_config(page_title="MNIST Digit Classifier", page_icon="✍️")
 
 # ------------------------
-# 1. Build or Load Model
+# 1. Load Pre-trained Model
 # ------------------------
 @st.cache_resource
 def get_model():
-    try:
-        # لو موجود model مدرب، نعمل load
-        model = load_model("mnist_cnn.h5")
-    except:
-        # لو مش موجود ندرب واحد صغير
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        x_train = x_train.reshape(-1, 28, 28, 1).astype("float32") / 255.0
-        x_test = x_test.reshape(-1, 28, 28, 1).astype("float32") / 255.0
-        y_train = to_categorical(y_train, 10)
-        y_test = to_categorical(y_test, 10)
-
-        model = Sequential([
-            Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
-            MaxPooling2D((2,2)),
-            Conv2D(64, (3,3), activation='relu'),
-            MaxPooling2D((2,2)),
-            Flatten(),
-            Dense(128, activation='relu'),
-            Dense(10, activation='softmax')
-        ])
-
-        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-        model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test))
-        model.save("mnist_cnn.h5")
+    model = load_model("myModelMnist.keras")
     return model
 
 model = get_model()
